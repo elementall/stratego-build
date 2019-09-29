1f7ef24b96f7eab31f04b8ebf5e5e2426f574fe7
B.Brunink <bas.brunink@hva.nl>
2019-09-29 14:28:49 +0200

    Bootstrap 4 toegevoegd....

--- stratego-frontend/src/app/app.module.ts
+++ stratego-frontend/src/app/app.module.ts
@@ -4,14 +4,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BoardComponent } from './components/game/board/board.component';
-import {GameService} from "./services/game/game.service";
+import {GameService} from './services/game/game.service';
 import { StockComponent } from './components/game/stock/stock.component';
+import { WelcomeComponent } from './components/welcome/welcome/welcome.component';
+import { LoginComponent } from './components/welcome/login/login.component';
+import { SignupComponent } from './components/welcome/signup/signup.component';
+import { HeaderComponent } from './components/common/header/header.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BoardComponent,
-    StockComponent
+    StockComponent,
+    WelcomeComponent,
+    LoginComponent,
+    SignupComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
