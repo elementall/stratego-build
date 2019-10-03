d51eb956f7438aa9be9ec13d5b3ce3557b46f9ab
D. Dekker <dave.dekker3@hva.nl>
2019-10-03 18:53:59 +0200

    HTML and CSS for the Lobby

--- stratego-frontend/src/app/components/lobby/lobby.component.ts
+++ stratego-frontend/src/app/components/lobby/lobby.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {DataService} from '../../services/data.service';
+import {Game} from '../../models/game.model';
 
 @Component({
   selector: 'app-lobby',
@@ -7,10 +8,18 @@ import {DataService} from '../../services/data.service';
   styleUrls: ['./lobby.component.css']
 })
 export class LobbyComponent implements OnInit {
+  games: Game[] = [];
+  row: number;
 
   constructor(private dataService: DataService) { }
 
   ngOnInit() {
+    this.games = this.dataService.games;
+  }
+
+
+
+  onSelected(game: Game, i: number) {
   }
 
 }
